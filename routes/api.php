dfda607d2cfee832d7e38a9e94c819443d128957
Dani Andhika Permana <daniandhika03@gmail.com>
2023-02-16 09:46:45 +0700

    Added register login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:api', 'role'])->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout']);
+
+    Route::middleware(['scope:admin,user'])->get('/user', function (Request $request) {
+        $user = $request->user();
+        $user->role = $user->role()->first()->role;
+        return $user;
+    });
 });
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [LoginController::class, 'login']);
